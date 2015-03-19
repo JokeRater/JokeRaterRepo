bdce1eb6215b5b628fe93041bffbf8737fc7e98f
1102085a <1102085A@student.gla.ac.uk>
2015-03-19 17:30:32 +0000

    fixed tets imports

--- JokeRaterProject/Tests.py
+++ JokeRaterProject/Tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from JokeRaterApp.models import Joke
+from django.contrib.auth.models import User
+from datetime import date, timedelta
+import datetime
 
 
 class JokeRaterTestCase(TestCase):
