ab0dce7562d51220b96911b813f904576303c371
1102085a <1102085A@student.gla.ac.uk>
2015-03-23 12:45:21 +0000

    More comprehensive tests implemented

--- JokeRaterProject/Tests.py
+++ JokeRaterProject/Tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from JokeRaterApp.models import Joke
+from JokeRaterApp.models import Joke, Category
 from django.contrib.auth.models import User
 from datetime import date, timedelta
 import datetime
@@ -7,17 +7,52 @@ import datetime
 
 class JokeRaterTestCase(TestCase):
     def setUp(self):
-        User.objects.create_user("TestUser", "testemail@example.com", "testpassword")
 
-        Joke.objects.create(category=doctor, content="Doctor, Doctor my son has swallowed my pen, what should I do?",
-                            punchline="Use a pen 'til I get there.", rating="6", postingUser=tesUser,
-                            datePosted=datetime.datetime.now().date())
+        #setup is also a test to evaluate creating and populating users, categories and jokes
+        user = User.objects.create_user("testUser", "testemail@example.com", "testpassword")
+        user.save
+
+        doctor = Category(name="Doctor-Doctor")
+        pun = Category(name="Puns")
+        doctor.save()
+        pun.save()
+
+        joke1 = Joke(category=doctor, content="Patient: Doctor, Doctor my son swallowed a pen!",
+                     punchline="Doctor: Use a pencil til I get there.", rating="6", postingUser=user,
+                     datePosted=datetime.datetime.now().date())
+        joke2 = Joke(category=pun, content="Did you hear about the guy who's left side was cut off?",
+                     punchline="He's all right now.", rating="3", postingUser=user,
+                     datePosted=datetime.date(2014, 8, 4))
+        joke1.save()
+        joke2.save()
+
 
     def test_content(self):
-        user = User.objects.get(name="TestUser")
-        joke = Joke.objects.get(punchline="Use a pen 'til I get there.")
+        # get
+        user = User.objects.get(username="testUser")
+        doctor = Category.objects.get(name="Doctor-Doctor")
+        pun = Category.objects.get(name="Puns")
+        jokeA = Joke.objects.get(punchline="Doctor: Use a pencil til I get there.")
+        jokeB = Joke.objects.get(punchline="He's all right now.")
 
+        # assert user attributes
+        self.assertEqual(user.username, "testUser")
         self.assertEqual(user.email, "testemail@example.com")
-        self.assertEqual(joke.rating, "6")
-		
-		
+        # testpassword is encoded
+
+        # assert category
+        self.assertEqual(doctor.name, "Doctor-Doctor")
+        self.assertEqual(pun.name, "Puns")
+
+        # assert joke attributes
+        self.assertEqual(jokeA.punchline, "Doctor: Use a pencil til I get there.")
+        self.assertEqual(jokeA.rating, 6)
+        self.assertEqual(jokeB.content, "Did you hear about the guy who's left side was cut off?")
+        self.assertEqual(jokeA.datePosted, datetime.datetime.now().date())
+        self.assertEqual(jokeB.datePosted, datetime.date(2014, 8, 4))
+        #assert postingUser object
+        self.assertEqual(jokeA.postingUser, user)
+        self.assertEqual(jokeB.postingUser, user)
+        #assert categories
+        self.assertEqual(jokeA.category, doctor)
+        self.assertEqual(jokeB.category, pun)
